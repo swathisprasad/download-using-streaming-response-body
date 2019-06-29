384dc2cfa9a9c0d6fcf11de8b157f3e39b26068b
Swathi Prasad <swathi.prasad88@gmail.com>
2019-06-29 23:25:35 +0200

    Configure async support for Spring MVC.

---
+++ src/main/java/com/techshard/download/AsyncConfiguration.java
@@ -0,0 +1,68 @@
+package com.techshard.download;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.async.CallableProcessingInterceptor;
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.Callable;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    private final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
+
+    @Override
+    @Bean (name = "taskExecutor")
+    public AsyncTaskExecutor getAsyncExecutor() {
+        log.debug("Creating Async Task Executor");
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+    /** Configure async support for Spring MVC. */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurerConfigurer(AsyncTaskExecutor taskExecutor, CallableProcessingInterceptor callableProcessingInterceptor) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+                configurer.setDefaultTimeout(3600000).setTaskExecutor(taskExecutor);
+                configurer.registerCallableInterceptors(callableProcessingInterceptor);
+                WebMvcConfigurer.super.configureAsyncSupport(configurer);
+            }
+        };
+    }
+
+    @Bean
+    public CallableProcessingInterceptor callableProcessingInterceptor() {
+        return new TimeoutCallableProcessingInterceptor() {
+            @Override
+            public <T> Object handleTimeout(NativeWebRequest request, Callable<T> task) throws Exception {
+                log.error("timeout!");
+                return super.handleTimeout(request, task);
+            }
+        };
+    }
+}
--- src/main/java/com/techshard/download/DownloadApplication.java
+++ src/main/java/com/techshard/download/DownloadApplication.java
@@ -3,8 +3,10 @@ package com.techshard.download;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class DownloadApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
